ae04171ac5fbf82905a29e0d2ae4c14a66f5b047
munashex <munashemugondaa@gmail.com>
2023-08-31 19:09:14 +0200

    updating api url

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -17,7 +17,7 @@ interface CoinProps {
 
 function Home() {
   const url =
-    'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false';
+    'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false';
 
   const [loading, setLoading] = useState(false);
   const [coins, setCoins] = useState<CoinProps[]>([]);
